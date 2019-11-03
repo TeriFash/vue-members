eb15e75411e23a6354b1b276e3af0ee9f1ee5f29
TeriFash <teri.fash@gmail.com>
2019-11-03 12:43:16 +0200

    add status task

--- src/views/AppList.vue
+++ src/views/AppList.vue
@@ -4,19 +4,19 @@
             <h3>List</h3>
             <hr>
         </div>
-        <div v-if="tasks.length > 0" class="col s4">
-            <div class="card">
+        <div v-if="tasks.length > 0" class="col s4" v-for="(task, i) in tasks">
+            <div class="card" :key="i">
                 <div class="card-image waves-effect waves-block waves-light">
                     <img class="activator" src="../assets/images/1.jpg">
                 </div>
                 <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4">Card Title<i
+                    <span class="card-title activator grey-text text-darken-4"><i class="material-icons left" :class="status ? 'is-active' : 'is-done'">{{ status ? 'history' : 'check_circle' }}</i>{{task.title}}<i
                         class="material-icons right">more_vert</i></span>
                     <p><a href="#">This is a link</a></p>
                 </div>
                 <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                    <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                    <span class="card-title grey-text text-darken-4">{{task.title}}<i class="material-icons right">close</i></span>
+                    <p>{{task.description}}</p>
                 </div>
             </div>
         </div>
@@ -31,6 +31,11 @@
         name: 'app-list',
         components: {
             NotFound
+        },
+        data() {
+            return {
+                status: true
+            };
         },
         computed: {
             tasks() {
