434b2ecfbd682cf3c4c6a7de5dda40156adc9919
TeriFash <teri.fash@gmail.com>
2019-11-03 17:19:14 +0200

    add random name

--- src/views/AppList.vue
+++ src/views/AppList.vue
@@ -4,33 +4,17 @@
             <h3>List</h3>
             <hr>
         </div>
-        <div v-if="tasks.length > 0" class="col s4" v-for="(task, i) in tasks">
-            <div class="card" :key="i">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src="../assets/images/1.jpg">
-                </div>
-                <div class="card-content">
-                    <span class="card-title activator grey-text text-darken-4"><i class="material-icons left" :class="status ? 'is-active' : 'is-done'">{{ status ? 'history' : 'check_circle' }}</i>{{task.title}}<i
-                        class="material-icons right">more_vert</i></span>
-                    <p><a href="#">This is a link</a></p>
-                </div>
-                <div class="card-reveal">
-                    <span class="card-title grey-text text-darken-4">{{task.title}}<i class="material-icons right">close</i></span>
-                    <p>{{task.description}}</p>
-                </div>
-            </div>
-        </div>
-        <not-found v-else />
+        <card :items="tasks" />
     </div>
 </template>
 
 <script>
-    import NotFound from '@/components/NotFound'
+    import Card from '@/components/Card'
 
     export default {
         name: 'app-list',
         components: {
-            NotFound
+            Card
         },
         data() {
             return {
